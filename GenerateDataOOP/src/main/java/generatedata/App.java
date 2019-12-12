58b8c34c35f1587b2ca1e848edd5a3060b9a17bd
ao chao <giangtn@aimesoft.com>
2019-12-12 14:20:21 +0700

    fix code in class GenerateFact

--- GenerateDataOOP/src/main/java/generatedata/App.java
+++ GenerateDataOOP/src/main/java/generatedata/App.java
@@ -9,6 +9,9 @@ import generatedata.fact.fact.GenerateFact;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
 import generatedata.entity.aggrement.GenerateAggrement;
 import generatedata.entity.country.GenerateCountry;
 import generatedata.entity.event.GenerateEvent;
@@ -35,86 +38,13 @@ public class App
         GenerateOrganization genOr = new GenerateOrganization();
         genOr.dump2Json("organization.json", 5);
 
-        List<String> relationships = new ArrayList<String>(){
-            {
-                add("Gặp gỡ");
-                add("Đi chơi cùng");
-                add("Ủng hộ");
-                add("Ủng hộ");
-                add("Phản đối");
-                add("Tổ chức");
-                add("Kết hợp");
-                add("Đàm phán");
-                add("Liên minh");
-                add("Tham gia");
-                add("Đi");
-                add("Chiến tranh");
-                add("Gặp gỡ");
-                add("Tổ chức tại");
-                add("Đi");
-            }
-        };
-        
-
-        List<String> subjects = new ArrayList<String>() {
-            {
-                add("person.json");
-                add("person.json");
-                add("organization.json");
-                add("country.json");
-                add("country.json");
-                add("organization.json");
-                add("person.json");
-                add("organization.json");
-                add("organization.json");
-                add("person.json");
-                add("person.json");
-                add("country.json");
-                add("person.josn");
-                add("event.json");
-                add("person.json");
-            }
-        };
-
-        List<String> objects = new ArrayList<String>() {
-            {
-                add("person.json");
-                add("person.json");
-                add("aggrement.json");
-                add("aggrement.json");
-                add("aggrement.json");
-                add("event.json");
-                add("organization.json");
-                add("organization.json");
-                add("organization.json");
-                add("organization.json");
-                add("event.json");
-                add("event.json");
-                add("person.josn");
-                add("location.json");
-                add("location.json");
-            }
-        };
-
         GenerateFact genFact = new GenerateFact();
-        for (int i = 0; i < relationships.size(); ++i) {
-            
-            genFact.setRelationship(relationships.get(i));
-            genFact.setSubjectPath(subjects.get(i));
-            genFact.setObjectPath(objects.get(i));
-
-            System.out.println(i);
-            
-            genFact.generate(10);
-        }
+        genFact.generate(10);
         genFact.dump2Json("fact.json");
-
 
         GenerateArticle genArt = new GenerateArticle();
         genArt.generate("fact.json");
-        genArt.dump2Json("article.json");
-        
-        
+        genArt.dump2Json("article.json"); 
 
     }
 }
--- GenerateDataOOP/src/main/java/generatedata/fact/fact/GenerateFact.java
+++ GenerateDataOOP/src/main/java/generatedata/fact/fact/GenerateFact.java
@@ -7,7 +7,9 @@ import java.io.FileOutputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Random;
 
 import com.google.gson.Gson;
@@ -20,38 +22,97 @@ import generatedata.utilites.RandomDate;
 public class GenerateFact {
 
     public JsonArray result;
-    private String relationship;
-    private String subjectPath, objectPath;
+    private List<String> relationship;
+    private List<String> subjectPath, objectPath;
 
     public GenerateFact() {
-        result = new JsonArray();
+        this.result = new JsonArray();
+        this.relationship = new ArrayList<String>(){
+            {
+                add("Gặp gỡ");
+                add("Đi chơi cùng");
+                add("Ủng hộ");
+                add("Ủng hộ");
+                add("Phản đối");
+                add("Tổ chức");
+                add("Kết hợp");
+                add("Đàm phán");
+                add("Liên minh");
+                add("Tham gia");
+                add("Đi");
+                add("Chiến tranh");
+                add("Gặp gỡ");
+                add("Tổ chức tại");
+                add("Đi");
+            }
+        };
+        
+        this.subjectPath = new ArrayList<String>() {
+            {
+                add("person.json");
+                add("person.json");
+                add("organization.json");
+                add("country.json");
+                add("country.json");
+                add("organization.json");
+                add("person.json");
+                add("organization.json");
+                add("organization.json");
+                add("person.json");
+                add("person.json");
+                add("country.json");
+                add("person.json");
+                add("event.json");
+                add("person.json");
+            }
+        };
+
+        this.objectPath = new ArrayList<String>() {
+            {
+                add("person.json");
+                add("person.json");
+                add("aggrement.json");
+                add("aggrement.json");
+                add("aggrement.json");
+                add("event.json");
+                add("organization.json");
+                add("organization.json");
+                add("organization.json");
+                add("organization.json");
+                add("event.json");
+                add("event.json");
+                add("person.json");
+                add("location.json");
+                add("location.json");
+            }
+        };
     }
 
-    public String getRelationship() {
+    public List<String> getRelationship() {
         return relationship;
     }
 
-    public String getObjectPath() {
+    public List<String> getObjectPath() {
         return objectPath;
     }
 
-    public void setObjectPath(String objectPath) {
+    public void setObjectPath(List<String> objectPath) {
         this.objectPath = objectPath;
     }
 
-    public String getSubjectPath() {
+    public List<String> getSubjectPath() {
         return subjectPath;
     }
 
-    public void setSubjectPath(String subjectPath) {
+    public void setSubjectPath(List<String> subjectPath) {
         this.subjectPath = subjectPath;
     }
 
-    public void setRelationship(String relationship) {
+    public void setRelationship(List<String> relationship) {
         this.relationship = relationship;
     }
 
-    JsonArray genFact(JsonArray subjects, JsonArray objects, int length) {
+    JsonArray genFact(JsonArray subjects, JsonArray objects, String relationship, int length) {
         JsonArray res = new JsonArray();
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
 
@@ -69,7 +130,7 @@ public class GenerateFact {
             obj.addProperty("id", "fact" + Integer.toString(i));
             obj.addProperty("relationship", relationship);
             obj.addProperty("subject_id", subjectId);
-            obj.addProperty("object", objectId);
+            obj.addProperty("object_id", objectId);
             obj.addProperty("extract_time", formatter.format(date));
             obj.addProperty("time", time);
 
@@ -79,19 +140,17 @@ public class GenerateFact {
     }
 
     public void generate(int length) {
-        Gson gson = new Gson();
         ParseData parse = new ParseData();
-        try {
-            JsonArray subjects = parse.getDataFromJson(subjectPath);
-            JsonArray objects = parse.getDataFromJson(objectPath);
 
-            JsonArray res = genFact(subjects, objects, length);
+        int nbRela = subjectPath.size();
+        for (int i = 0; i < nbRela; ++i) {
+            JsonArray subjects = parse.getDataFromJson(subjectPath.get(i));
+            JsonArray objects = parse.getDataFromJson(objectPath.get(i));
+            String rela = relationship.get(i);
+
+            JsonArray res = genFact(subjects, objects, rela, length);
             result.addAll(res);
-
-        } catch (Exception e) {
-            //TODO: handle exception
         }
-        
     }
 
     public void dump2Json(String filePath) {
@@ -107,6 +166,5 @@ public class GenerateFact {
             e.printStackTrace();
         }
     }
-
 
 }
